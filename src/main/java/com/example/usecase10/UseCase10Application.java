9cbfe89aac00d9b44f395ff55e50454a7af2d691
Edisson David Prieto Vargas <v-edivargas@expediagroup.com>
2023-08-27 00:55:41 -0500

    UC#10 Infrastructure setup

---
+++ src/main/java/com/example/usecase10/UseCase10Application.java
@@ -0,0 +1,13 @@
+package com.example.usecase10;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UseCase10Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UseCase10Application.class, args);
+    }
+
+}
